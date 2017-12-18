78437ba3f093fee562655930fb43f9f34f74da3b
Petri Savolainen <petri.savolainen@koodaamo.fi>
2017-12-18 14:33:11 +0200

    Prepare koodaamo.changejournal 1.0a2.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='koodaamo.changejournal',
-    version='1.0a1',
+    version='1.0a2',
     description="Content change journaling",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
